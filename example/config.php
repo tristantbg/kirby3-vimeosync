d2fbec3d8faa838e157ef92cbb252cf2d406abd5
tristantbg <tristan.bagot@gmail.com>
2021-01-13 08:47:45 +0100

    Update config.php

--- example/config.php
+++ example/config.php
@@ -4,25 +4,25 @@ return [
     'bnomei.janitor.jobs' => [
         'vimeosync_getvideos'     => function (Kirby\Cms\Page $page = null, string $data = null) {
 
-            \Bnomei\Janitor::log('vimeosync_getvideos.mask ' . time());
-
             $job = \VimeoSync\App::getVideos();
 
-            \Bnomei\Janitor::log('vimeosync_getvideos.exit ' . time());
             return [
                 'status' => $job ? 200 : 404,
+                'reload' => true,
                 'label'  => 'Synced!',
             ];
         },
         'vimeosync_getthumbnails' => function (Kirby\Cms\Page $page = null, string $data = null) {
 
-            \Bnomei\Janitor::log('vimeosync_getthumbnails.mask ' . time());
+            if ($page->intendedTemplate() == 'vimeo.video') {
+              $job = \VimeoSync\App::getThumbnails($page);
+            } else {
+              $job = \VimeoSync\App::getThumbnails();
+            }
 
-            $job = \VimeoSync\App::getThumbnails();
-
-            \Bnomei\Janitor::log('vimeosync_getthumbnails.exit ' . time());
             return [
                 'status' => $job ? 200 : 404,
+                'reload' => true,
                 'label'  => 'Synced!',
             ];
         },
